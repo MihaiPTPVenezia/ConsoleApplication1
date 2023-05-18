80df988bab1c03327b7e40f7521ce2bef1a6dc5c
MIHAI.IACHIMOVSCHI <MIHAI.IACHIMOVSCHI@LABINFO20>
2023-05-18 15:20:49 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
